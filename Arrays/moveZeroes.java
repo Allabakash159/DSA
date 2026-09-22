13b2552d33b144e5ed4f212b3d0b74f77c3ef48c
Allabakash159 <149656343+Allabakash159@users.noreply.github.com>
2026-09-22 22:46:06 +0530

    Optimize zero movement in moveZeroes method
    Refactored moveZeroes method to optimize zero movement in the array.

--- Arrays/moveZeroes.java
+++ Arrays/moveZeroes.java
@@ -16,3 +16,27 @@ class Solution {
         }
     }
 }
+
+
+// one more approach is to use a copy of array
+// Best approach is the below one
+/*
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int w=0;
+        for (int r=0;r<nums.length;r++){
+           if(nums[r]!=0){
+            if(r!=w){
+            int temp=nums[r];
+            nums[r]=nums[w];
+            nums[w]=temp;
+            w++;
+            }
+           }
+        }
+    }
+}
+*/
+
+
+// In this approach I have just used one variable of w and when r,w at same non zero element without extra writing we are just moving forward, we are simply avoiding extra writes 
